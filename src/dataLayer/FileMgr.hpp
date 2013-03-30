1854359ec4c954dfd251f789b149c446b5a0d423
Nathan <nlutz91@gmail.com>
2013-03-30 09:59:37 -0400

    Comments and structure update
    Changed IEvent to give fewer conflicts in merges and updated spec for
    FileMgr.

--- src/dataLayer/FileMgr.hpp
+++ src/dataLayer/FileMgr.hpp
@@ -6,9 +6,15 @@
  * Ingredient Collection/Shopping List
  * Can add items to each list
  * Can remove items from each list.
+ * Can Request either list
  *
  * Recipe Collection
- *
+ * Can add recipes
+ * Can remove recipes
+ * Can edit recipes
+ * Can request specific
+ * Can request all (perhaps in chunks?)
+ * Can request by search terms
  */
 
 #ifndef FILEMGR_HPP_
@@ -19,16 +25,16 @@ public:
 	FileMgr();
 	virtual ~FileMgr();
 
-	//TODO void GetSearchResults();
+	//TODO void GetIngredientList(bool isShoppingList);
+	//TODO void AddToIngredientList(int recipeID, bool isShoppingList);
+	//TODO void RemoveFromIngredientList(int recipeID, bool isShoppingList);
 
-	//TODO void GetIngredientList(boolean shoppingList);
-	//TODO void AddToIngredientList();
-	//TODO void ModifyIngredientList();
-
-	//TODO void GetRecipe(int recipeID);
 	//TODO void AddRecipe();
-	//TODO void DeleteRecipe(int recipeID);
-	//TODO void ModifyRecipe();
+	//TODO void RemoveRecipe();
+	//TODO void EditRecipe();
+	//TODO void GetRecipe(int recipeID);
+	//TODO void GetAllRecipes();
+	//TODO void GetSearchResults();
 };
 
 #endif /* FILEMGR_HPP_ */
